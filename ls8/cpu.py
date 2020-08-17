a84181f80990aeaf18af8401f94ac6bebd62b663
Michael Perez <mperez6473@gmail.com>
2020-08-17 17:35:29 -0500

    reworked the way reg is loaded and ram is loaded later.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,5 @@
-"""CPU functionality."""
 
-import sys
+"""CPU functionality."""
 
 
 class CPU:
@@ -8,13 +7,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        print('Welcome, Now booting up Janky OS')
+
+        self.reg = [0] * 8
 
     def ram_read(self, location):
         return self.ram[location]
 
-    def ram_write(self, value, location):
-        return self.ram[location] = value
+    def ram_write(self, value, location,):
+        self.ram[location] = value
 
     def load(self):
         """Load a program into memory."""
@@ -32,10 +33,8 @@ class CPU:
             0b00000000,
             0b00000001,  # HLT
         ]
-        self.ram = [0]*8
-
         self.pc = 0
-
+        self.ram = [0]*255
         for instruction in program:
             self.ram[address] = instruction
             address += 1
@@ -51,7 +50,7 @@ class CPU:
 
     def trace(self):
         """
-        Handy function to print out the CPU state. 
+        Handy function to print out the CPU state.
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
@@ -69,4 +68,32 @@ class CPU:
         print()
 
     def run(self):
+
         self.trace()
+        # print(self.load())
+        print(f'Total reg: {self.reg} ')
+        print(f'Total RAM: {self.ram}')
+        print(f'Your pc: {self.pc}')
+
+        running = True
+        while running:
+
+            ir = self.ram[self.pc]
+            # print(self.reg, self.pc)
+            print(f'Current IR: {ir}')
+
+            if ir == 1:  # HLT -- Computer Halt (STOP)
+                print('COMPUTER STOPPED')
+                running = False
+                self.pc += 1
+            if ir == 130:   # LDI -- Add following item to ram
+                print(f' in LDI, self.pc: {self.pc} ')
+                self.pc += 3
+            if ir == 71:    # PRN -- Display next item from ram
+                print(self.pc, self.pc + 1, self.pc + 2)
+                print(self.ram[self.pc])
+                print(self.ram[self.pc+1])
+                print(self.ram[self.pc+2])
+                self.ram_read(self.pc+1)
+                print(f'self.ram_read(self.pc+1): {self.ram_read(self.pc+2)} ')
+                self.pc += 2
